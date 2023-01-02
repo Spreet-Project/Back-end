729bb8c7e0f73d0011978d46430d09f085affa16
starMinK <rbals040329@gmail.com>
2023-01-02 09:46:59 +0900

    Feat: spring boot build

---
+++ spreet/src/main/java/com/team1/spreet/SpreetApplication.java
@@ -0,0 +1,13 @@
+package com.team1.spreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreetApplication.class, args);
+	}
+
+}
---
+++ spreet/src/test/java/com/team1/spreet/SpreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.spreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
