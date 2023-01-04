5abb322ff75059717b5ededf5badeb6b97c08a09
starMinK <rbals040329@gmail.com>
2023-01-05 03:33:37 +0900

    Fix: user관련 exception handling

--- spreet/src/main/java/com/team1/spreet/repository/UserRepository.java
+++ spreet/src/main/java/com/team1/spreet/repository/UserRepository.java
@@ -7,4 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginId(String loginId);
+
+    Optional<User> findByNickname(String nickname);
+
+    boolean existsByLoginIdAndIsDeleted(String loginId, boolean isDeleted);
 }
