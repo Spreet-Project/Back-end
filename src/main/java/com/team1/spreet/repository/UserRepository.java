3dc03b429f791264edb2a3f3ab0e2511b1cce2ad
rara-dev <kksr1219@gmail.com>
2023-01-11 17:03:24 +0900

    Feat: Admin 기능 구현

--- src/main/java/com/team1/spreet/repository/UserRepository.java
+++ src/main/java/com/team1/spreet/repository/UserRepository.java
@@ -15,9 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 //    boolean existsByLoginIdAndIsDeleted(String loginId, boolean isDeleted);
 
-//    @Query("select u from User u where u.userRole = :userRole and u.isCrew = false")
-//    List<User> findByUserRoleAndCrewIsFalse(@Param(value = "userRole") UserRole userRole);
-
     List<User> findByUserRoleAndIsCrew(UserRole userRole, boolean isCrew);
 
     boolean existsByLoginIdAndUserRoleAndIsCrew(String loginId, UserRole userRole, boolean isCrew);
