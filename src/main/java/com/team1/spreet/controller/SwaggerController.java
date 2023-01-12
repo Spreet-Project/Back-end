a1ac684fd4f46ef282b80ace9db5f0805d821410
rara-dev <kksr1219@gmail.com>
2023-01-13 03:15:09 +0900

    Feat: Swagger 적용

--- src/main/java/com/team1/spreet/controller/SwaggerController.java
+++ src/main/java/com/team1/spreet/controller/SwaggerController.java
@@ -1,17 +1,20 @@
 package com.team1.spreet.controller;
 
+import io.swagger.annotations.Api;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "Swagger")
 @RestController
 @RequestMapping("/api")
 public class SwaggerController {
 	@GetMapping("/doc")
 	public void redirectSwagger(HttpServletResponse response) throws IOException {
+		response.sendRedirect("https://sparta-rara.shop/swagger-ui/index.html");
 //		response.sendRedirect("http://52.78.92.15/swagger-ui/index.html");
-		response.sendRedirect("http://localhost:8080/swagger-ui/index.html");
+//		response.sendRedirect("http://localhost:8080/swagger-ui/index.html");
 	}
 }
