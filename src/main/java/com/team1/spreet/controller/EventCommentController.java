d00b60627c910e73ac21a236db422fcaa5fc2ddb
이승열 <116939888+misracis2@users.noreply.github.com>
2023-01-19 15:56:26 +0900

    Fix: eventCommentController Api어노테이션 추가

--- src/main/java/com/team1/spreet/controller/EventCommentController.java
+++ src/main/java/com/team1/spreet/controller/EventCommentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = "eventComment")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/event")
