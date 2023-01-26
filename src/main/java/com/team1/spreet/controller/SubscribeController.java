ac835acc789f3001fa4eaba592cf4af587edca16
rara-dev <kksr1219@gmail.com>
2023-01-27 01:21:28 +0900

    Feat: swagger 적용

--- src/main/java/com/team1/spreet/controller/SubscribeController.java
+++ src/main/java/com/team1/spreet/controller/SubscribeController.java
@@ -4,21 +4,24 @@ import com.team1.spreet.dto.CustomResponseBody;
 import com.team1.spreet.exception.SuccessStatusCode;
 import com.team1.spreet.security.UserDetailsImpl;
 import com.team1.spreet.service.SubscribeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "subscribe")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/subscribe")
 public class SubscribeController {
     private final SubscribeService subscribeService;
     @PostMapping("/{publisherId}")
-    public CustomResponseBody<SuccessStatusCode> subscribe(@PathVariable Long publisherId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public CustomResponseBody<SuccessStatusCode> subscribe(@ApiParam(value = "구독을 요청한 회원 ID") @PathVariable Long publisherId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return new CustomResponseBody<>(subscribeService.subscribe(publisherId, userDetails));
     }
     @DeleteMapping("/{publisherId}")
-    public CustomResponseBody<SuccessStatusCode> cancelSubscribe(@PathVariable Long publisherId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public CustomResponseBody<SuccessStatusCode> cancelSubscribe(@ApiParam(value = "구독 취소를 요청한 회원 ID") @PathVariable Long publisherId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return new CustomResponseBody<>(subscribeService.cancelSubscribe(publisherId, userDetails));
     }
 }
