f128fdff3039a9dcb14898403ba40607ec845230
seungyeol lee <misracis@gmail.com>
2023-02-02 14:44:16 +0900

    refactor: subscribe api 요청데이터 변환(id -> nickname)으로 인한 Dto 생성 및 controller 코드 변경

--- src/main/java/com/team1/spreet/domain/subscribe/controller/SubscribeController.java
+++ src/main/java/com/team1/spreet/domain/subscribe/controller/SubscribeController.java
@@ -1,9 +1,10 @@
 package com.team1.spreet.domain.subscribe.controller;
 
+import com.team1.spreet.domain.subscribe.dto.SubscribeDto;
+import com.team1.spreet.domain.subscribe.service.SubscribeService;
+import com.team1.spreet.global.auth.security.UserDetailsImpl;
 import com.team1.spreet.global.common.dto.CustomResponseBody;
 import com.team1.spreet.global.common.model.SuccessStatusCode;
-import com.team1.spreet.global.auth.security.UserDetailsImpl;
-import com.team1.spreet.domain.subscribe.service.SubscribeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +17,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/subscribe")
 public class SubscribeController {
     private final SubscribeService subscribeService;
-    @PostMapping("/{publisherNickname}")
-    public CustomResponseBody<SuccessStatusCode> subscribe(@ApiParam(value = "구독을 요청한 회원 ID") @PathVariable String publisherNickname,
+    @PostMapping
+    public CustomResponseBody<SuccessStatusCode> subscribe(@ApiParam(value = "구독을 요청한 회원 ID") @RequestBody SubscribeDto.RequestDto requestDto,
                                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        subscribeService.subscribe(publisherNickname, userDetails.getUser());
+        subscribeService.subscribe(requestDto.getNickname(), userDetails.getUser());
         return new CustomResponseBody<>(SuccessStatusCode.SUBSCRIBE);
     }
-    @DeleteMapping("/{publisherNickname}")
-    public CustomResponseBody<SuccessStatusCode> cancelSubscribe(@ApiParam(value = "구독 취소를 요청한 회원 ID") @PathVariable String  publisherNickname,
+    @DeleteMapping
+    public CustomResponseBody<SuccessStatusCode> cancelSubscribe(@ApiParam(value = "구독 취소를 요청한 회원 ID") @RequestBody SubscribeDto.RequestDto requestDto,
                                                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        subscribeService.cancelSubscribe(publisherNickname, userDetails.getUser());
+        subscribeService.cancelSubscribe(requestDto.getNickname(), userDetails.getUser());
         return new CustomResponseBody<>(SuccessStatusCode.CANCEL_SUBSCRIBE);
     }
 }
