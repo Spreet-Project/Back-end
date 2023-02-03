049d7d1a267db737a470ba5bd8e139999be0bb1a
rara-dev <kksr1219@gmail.com>
2023-02-03 16:58:07 +0900

    Refactor: 유저 정보 확인 코드 리팩토링

--- src/main/java/com/team1/spreet/domain/event/controller/EventCommentController.java
+++ src/main/java/com/team1/spreet/domain/event/controller/EventCommentController.java
@@ -1,18 +1,22 @@
 package com.team1.spreet.domain.event.controller;
 
-import com.team1.spreet.global.common.dto.CustomResponseBody;
 import com.team1.spreet.domain.event.dto.EventCommentDto;
-import com.team1.spreet.global.common.model.SuccessStatusCode;
-import com.team1.spreet.global.auth.security.UserDetailsImpl;
 import com.team1.spreet.domain.event.service.EventCommentService;
+import com.team1.spreet.global.common.dto.CustomResponseBody;
+import com.team1.spreet.global.common.model.SuccessStatusCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "eventComment")
 @RestController
@@ -23,33 +27,34 @@ public class EventCommentController {
     private final EventCommentService eventCommentService;
     @ApiParam(value = "행사 댓글 등록 API")
     @PostMapping("/{eventId}/comment")
-    public CustomResponseBody<SuccessStatusCode> saveEventComment(@PathVariable @ApiParam(value = "댓글 등록할 행사 ID") Long eventId,
-                                               @RequestBody @Valid @ApiParam(value = "등록할 댓글 내용") EventCommentDto.RequestDto requestDto,
-                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        eventCommentService.saveEventComment(eventId, requestDto, userDetails.getUser());
+    public CustomResponseBody<SuccessStatusCode> saveEventComment(
+        @PathVariable @ApiParam(value = "댓글 등록할 행사 ID") Long eventId,
+        @RequestBody @Valid @ApiParam(value = "등록할 댓글 내용") EventCommentDto.RequestDto requestDto) {
+        eventCommentService.saveEventComment(eventId, requestDto);
         return new CustomResponseBody<>(SuccessStatusCode.SAVE_COMMENT);
     }
 
     @ApiParam(value = "행사 댓글 수정 API")
     @PutMapping("/comment/{commentId}")
-    public CustomResponseBody<SuccessStatusCode> updateEventComment(@PathVariable @ApiParam(value = "행사 댓글 ID") Long commentId,
-                                                 @RequestBody @Valid @ApiParam(value = "수정할 댓글 내용") EventCommentDto.RequestDto requestDto,
-                                                 @AuthenticationPrincipal UserDetailsImpl userDetails){
-        eventCommentService.updateEventComment(commentId, requestDto, userDetails.getUser());
+    public CustomResponseBody<SuccessStatusCode> updateEventComment(
+        @PathVariable @ApiParam(value = "행사 댓글 ID") Long commentId,
+        @RequestBody @Valid @ApiParam(value = "수정할 댓글 내용") EventCommentDto.RequestDto requestDto){
+        eventCommentService.updateEventComment(commentId, requestDto);
         return new CustomResponseBody<>(SuccessStatusCode.UPDATE_COMMENT);
     }
 
     @ApiParam(value = "행사 댓글 삭제 API")
     @DeleteMapping("/comment/{commentId}")
-    public CustomResponseBody<SuccessStatusCode> deleteEventComment(@PathVariable @ApiParam(value = "행사 댓글 ID") Long commentId,
-                                                                    @AuthenticationPrincipal UserDetailsImpl userDetails){
-        eventCommentService.deleteEventComment(commentId, userDetails.getUser());
+    public CustomResponseBody<SuccessStatusCode> deleteEventComment(
+        @PathVariable @ApiParam(value = "행사 댓글 ID") Long commentId){
+        eventCommentService.deleteEventComment(commentId);
         return new CustomResponseBody<>(SuccessStatusCode.DELETE_COMMENT);
     }
 
     @ApiParam(value = "행사 댓글 조회 API")
     @GetMapping("/{eventId}/comment")
-    public CustomResponseBody<List<EventCommentDto.ResponseDto>> getEventCommentList(@PathVariable @ApiParam(value = "댓글 리스트를 조회할 행사 ID") Long eventId){
+    public CustomResponseBody<List<EventCommentDto.ResponseDto>> getEventCommentList(
+        @PathVariable @ApiParam(value = "댓글 리스트를 조회할 행사 ID") Long eventId){
         return new CustomResponseBody<>(SuccessStatusCode.GET_COMMENTS, eventCommentService.getEventCommentList(eventId));
     }
 
--- src/main/java/com/team1/spreet/domain/event/controller/EventController.java
+++ src/main/java/com/team1/spreet/domain/event/controller/EventController.java
@@ -1,17 +1,15 @@
 package com.team1.spreet.domain.event.controller;
 
-import com.team1.spreet.global.common.dto.CustomResponseBody;
 import com.team1.spreet.domain.event.dto.EventDto;
-import com.team1.spreet.global.common.model.SuccessStatusCode;
-import com.team1.spreet.global.auth.security.UserDetailsImpl;
 import com.team1.spreet.domain.event.service.EventService;
+import com.team1.spreet.global.common.dto.CustomResponseBody;
+import com.team1.spreet.global.common.model.SuccessStatusCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -32,35 +30,36 @@ public class EventController {
 	// Event 게시글 등록
 	@ApiOperation(value = "행사 게시글 등록 API")
 	@PostMapping
-	public CustomResponseBody<SuccessStatusCode> saveEvent(@ModelAttribute @Valid @ApiParam(value = "행사 게시글 등록 정보")
-		EventDto.RequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		eventService.saveEvent(requestDto, userDetails.getUser());
+	public CustomResponseBody<SuccessStatusCode> saveEvent(
+		@ModelAttribute @Valid @ApiParam(value = "행사 게시글 등록 정보") EventDto.RequestDto requestDto) {
+		eventService.saveEvent(requestDto);
 		return new CustomResponseBody<>(SuccessStatusCode.SAVE_EVENT);
 	}
 
 	// Event 게시글 수정
 	@ApiOperation(value = "행사 게시글 수정 API")
 	@PutMapping("/{eventId}")
-	public CustomResponseBody<SuccessStatusCode> updateEvent(@ModelAttribute @Valid @ApiParam(value = "행사 게시글 수정 정보")
-		EventDto.UpdateRequestDto requestDto, @PathVariable @ApiParam(value = "수정할 행사 게시글 ID")Long eventId,
-		@AuthenticationPrincipal UserDetailsImpl userDetails) {
-		eventService.updateEvent(requestDto, eventId, userDetails.getUser());
+	public CustomResponseBody<SuccessStatusCode> updateEvent(
+		@ModelAttribute @Valid @ApiParam(value = "행사 게시글 수정 정보") EventDto.UpdateRequestDto requestDto,
+		@PathVariable @ApiParam(value = "수정할 행사 게시글 ID")Long eventId) {
+		eventService.updateEvent(requestDto, eventId);
 		return new CustomResponseBody<>(SuccessStatusCode.UPDATE_EVENT);
 	}
 
 	// Event 게시글 삭제
 	@ApiOperation(value = "행사 게시글 삭제 API")
 	@DeleteMapping("/{eventId}")
-	public CustomResponseBody<SuccessStatusCode> deleteEvent(@PathVariable @ApiParam(value = "삭제할 행사 게시글 ID") Long eventId,
-		@AuthenticationPrincipal UserDetailsImpl userDetails) {
-		eventService.deleteEvent(eventId, userDetails.getUser());
+	public CustomResponseBody<SuccessStatusCode> deleteEvent(
+		@PathVariable @ApiParam(value = "삭제할 행사 게시글 ID") Long eventId) {
+		eventService.deleteEvent(eventId);
 		return new CustomResponseBody<>(SuccessStatusCode.DELETE_EVENT);
 	}
 
 	// Event 게시글 상세조회
 	@ApiOperation(value = "행사 게시글 상세조회 API")
 	@GetMapping("/{eventId}")
-	public CustomResponseBody<EventDto.ResponseDto> getEvent(@PathVariable @ApiParam(value = "조회할 행사 게시글 ID") Long eventId) {
+	public CustomResponseBody<EventDto.ResponseDto> getEvent(
+		@PathVariable @ApiParam(value = "조회할 행사 게시글 ID") Long eventId) {
 		return new CustomResponseBody<>(SuccessStatusCode.GET_EVENT, eventService.getEvent(eventId));
 	}
 
